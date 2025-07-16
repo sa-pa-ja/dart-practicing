8475ff483757a3ff5d78efa58ccc7b1883f864cb
Saturnino <161881@unsaac.edu.pe>
2025-07-16 08:22:20 -0500

    Second commit

--- index.dart
+++ index.dart
@@ -1,3 +1,26 @@
 void main() {
-  print("Hello world!!");
+  // print(Add());
+  // int age = 50;
+  // double width = 50.60;
+  // bool married = true;
+  // String message = "Hello my love Lucy";
+  // print("name: $age and married: $married the climate: $degrees");
+  // print(width);
+  print(a + b);
+  print(a * b);
+  print(a / b);
+  print(a == b);
+  print(a != b);
+  print(a < b);
+  print(a > b);
 }
+
+// int Add() {
+//   return 40 + 70;
+// }
+
+// int degrees = 50;
+
+
+int a = 60;
+int b = 6000;
